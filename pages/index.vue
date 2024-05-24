a772a0f3f48e0443bc6e9c9c51652b26ab1c4c25
Aaron Spurlock <122815347+mhiajs@users.noreply.github.com>
2024-05-24 18:27:06 -0500

    2024-05-24 18:27:06

--- pages/index.vue
+++ pages/index.vue
@@ -44,9 +44,6 @@
 </template>
 
 <script lang="ts" setup>
-import { useRouter, useRoute } from "vue-router";
-import { ref, reactive } from "vue";
-
 const router = useRouter();
 const route = useRoute();
 const isLoading = ref(false);
@@ -67,11 +64,11 @@ interface UserObject {
     };
 }
 
-const login = async () => {
+async function login() {
     isLoading.value = true;
 
     try {
-        const { data, error } = await useFetch<UserObject>(`https://users.evergreenmhi.net/auth/login`, {
+        const resp = await $fetch<UserObject>(`https://users.evergreenmhi.net/auth/login`, {
             method: "POST",
             body: JSON.stringify(form)
         });
@@ -82,43 +79,37 @@ const login = async () => {
             return process.env.NODE_ENV === "production";
         });
 
-        if (data.value?.ok) {
-            const userTokenCookie = useCookie("evergreen_u", {
-                path: "/",
-                maxAge: 60 * 60 * 24, // 24 hours
-                ...(inProduction.value && { domain: ".evergreenmhi.com" }),
-                ...(inProduction.value && { secure: true }),
-                sameSite: "lax"
-            });
 
-            userTokenCookie.value = JSON.stringify({ token: data.value.token, user: data.value.user });
+        const userTokenCookie = useCookie("evergreen_u", {
+            path: "/",
+            maxAge: 60 * 60 * 24, // 24 hours
+            ...(inProduction.value && { domain: ".evergreenmhi.com" }),
+            ...(inProduction.value && { secure: true }),
+            sameSite: "lax"
+        });
 
-            const redirectUrl: string = (route.query.redirect as string) || "https://five.evergreenmhi.com";
+        userTokenCookie.value = JSON.stringify({ token: resp.token, user: resp.user });
 
-            // Check if the URL is external
-            const isExternal = (url: string) => {
-                try {
-                    const u = new URL(url);
-                    return u.hostname !== window.location.hostname;
-                } catch {
-                    // If URL isn't valid return false
-                    return false;
-                }
-            };
+        const redirectUrl: string = (route.query.redirect as string) || "https://five.evergreenmhi.com";
 
-            if (isExternal(redirectUrl)) {
-                // If the URL is external, set window.location.href
-                window.location.replace(decodeURIComponent(redirectUrl));
-            } else {
-                // Otherwise, use router.push for internal navigation
-                await router.replace(decodeURIComponent(redirectUrl));
+        // Check if the URL is external
+        const isExternal = (url: string) => {
+            try {
+                const u = new URL(url);
+                return u.hostname !== window.location.hostname;
+            } catch {
+                // If URL isn't valid return false
+                return false;
             }
+        };
+
+        if (isExternal(redirectUrl)) {
+            // If the URL is external, set window.location.href
+            window.location.replace(decodeURIComponent(redirectUrl));
         } else {
-            console.error(error.value?.message);
+            // Otherwise, use router.push for internal navigation
+            await router.replace(decodeURIComponent(redirectUrl));
         }
-    } catch (err) {
-        isLoading.value = false;
-        console.trace(err);
     }
-};
+}
 </script>
